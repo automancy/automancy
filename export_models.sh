46cf8edbe4135c6e9abb3a153964f03b3cacaa1a
Madeline Sparkles <sparkles.system@gmail.com>
2023-02-01 17:10:58 +0800

    improved export script -- thanks Sylvie :3
    Former-commit-id: bf5019258c9c7b13adb01d4c7b6b8e14d68102b2

--- export_models.sh
+++ export_models.sh
@@ -2,9 +2,14 @@
 
 run_blender() {
   path=$1
-  blender "$path" --background --python export_models.py -- "${path%.*}.ply"
+  out_path="${path%.*}.ply"
+
+  #    vvvv doesn't exist or               vvv older than
+  if [ ! -f "$out_path" ] || [ "$out_path" -ot "$path" ]; then
+    blender "$path" --background --python export_models.py -- "$out_path"
+  fi
 }
 
 export -f run_blender
 
-find ./resources -name "**.blend" -exec bash -c 'run_blender "$0"' {} \;
+find ./resources -name "**.blend" | parallel run_blender {}
