48a98ae764f45a011077a448998c6b7037b64e2c
Madeline Sparkles <madeline@mouse.lgbt>
2023-06-25 14:58:46 +0800

    model
    Signed-off-by: Madeline Sparkles <madeline@mouse.lgbt>
    
    Former-commit-id: 2e0429bc1e9271140fbb89aad02828af1e83d8cb

--- export_models.py
+++ export_models.py
@@ -8,4 +8,4 @@ for object in bpy.data.objects:
     edge_split.split_angle = 0.0
     object.modifiers.new(name="Triangulate", type='TRIANGULATE')
 
-bpy.ops.export_mesh.ply(filepath=sys.argv[-1], use_uv_coords=False, axis_forward='-Y', use_ascii=True)
+bpy.ops.export_mesh.ply(filepath=sys.argv[-1], use_uv_coords=False, axis_forward='-Y')
