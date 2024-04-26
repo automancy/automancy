5d9977471e893d215fca91aa37e918bbabd510a9
Madeline Sparkles <madeline@mouse.lgbt>
2024-04-27 05:00:53 +0800

    fix research unlock being inverted

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -17,7 +17,7 @@ pub fn is_research_unlocked(
         .entry(resource_man.registry.data_ids.unlocked_researches)
         .or_insert_with(|| Data::SetId(HashSet::new()))
     {
-        if !unlocked.contains(&research) {
+        if unlocked.contains(&research) {
             return true;
         }
     }
